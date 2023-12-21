18a7b37ed4e846b1ed18ad087a5bcf1732d9f118
Alfaaprian <149498311+Alfaaprian@users.noreply.github.com>
2023-12-22 00:39:24 +0700

    Add files via upload

---
+++ UAS-alfa/README.md
@@ -0,0 +1,14 @@
+# Tugas UAS SPK
+
+Nama : Alfa Saputra <br>
+NIM : 201011400588 <br>
+Kelas : 07-TPLP-012 <br>
+
+## Install requirements
+
+`pip install -r requirements.txt`
+
+## Run the app
+
+Untuk menjalankan:
+`python main.py`
---
+++ UAS-alfa/alfa_SPK_Model/alfa.SQl.sql
@@ -0,0 +1,11 @@
+INSERT INTO smartphonexiaomi (brand,ram,prosesor,storage,baterai,harga,ukuran_layar) VALUES
+	 ('Xioami C11','2 GB','MediaTek Helio G35','32 GB','5.000 mAh','Rp.1.499.000','6.5 inch'),
+	 ('Xioami C15','4 GB','MediaTek Helio G35','64 GB','6.000 mAh','Rp.1.999.000','6.5 inch'),
+	 ('Xioami 8 Pro','8 GB','Qualcomm Snapdragon 720G','128 GB','4.500 mAh','Rp.3.999.000','6.4 inch'),
+	 ('Xioami X7 Pro','8 GB','MediaTek Dimensity 1000+','128 GB','4.500 mAh','Rp.4.999.000','6.55 inch'),
+	 ('Xioami GT Neo','8 GB','MediaTek Dimensity 1200','128 GB','4.500 mAh','Rp.4.999.000','6.43 inch'),
+	 ('Xioami X50 Pro','12 GB','Qualcomm Snapdragon 865','128 GB','4.200 mAh','Rp.7.999.000','6.44 inch'),
+	 ('Xioami Narzo 50A','4 GB','MediaTek Helio G85','64 GB','6.000 mAh','Rp.2.499.000','6.5 inch'),
+	 ('Xioami 9','8 GB','MediaTek Dimensity 810','128 GB','5.000 mAh','Rp.3.999.000','6.5 inch'),
+	 ('Xioami X9 Pro','12 GB','Qualcomm Snapdragon 870','128 GB','4.500 mAh','Rp.5.999.000','6.55 inch'),
+	 ('Xioami GT 2','12 GB','Qualcomm Snapdragon 898','128 GB','5.000 mAh','Rp.5.970.000','6.62 inch');
